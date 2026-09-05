c29b270c33c3e32328eb99e1a96a8b7c7e89e70b
Dissel <camilodissel@gmail.com>
2026-09-04 19:04:10 -0500

    Funcion lambda

---
+++ Funciones/funciones_lambda.py
@@ -0,0 +1,19 @@
+numeros = [1,2,3,4,5,6,7,8,9,10,13,14,20]
+
+#creando una fucnion lambda para multiplicar por 2
+#multiplicar_por_dos = lambda x : x*2
+
+#Crando funcion comun que diga si es par o no
+def es_par(num):
+    if(num%2==0):
+        return True
+    
+#Usando filter con una funcion comun
+numeros_pares = filter(es_par,numeros)
+
+#Creando lo mismo con lambda
+numeros_pares = filter(lambda numero:numero%2 == 0,numeros)
+
+
+print(list(numeros_pares))
+
